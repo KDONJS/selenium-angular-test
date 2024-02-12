555b3b49550ec0866719a699a1164928de1bc4c3
Yorlin Quispe Ygnacio <yquispey@bcds.net>
2024-02-11 22:06:58 -0500

    first commit-v2

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,6 +9,7 @@ import { Router } from '@angular/router'; // Importa el Router
 })
 export class LoginComponent {
   loginForm: FormGroup;
+  public error: boolean = false; 
 
   constructor(private formBuilder: FormBuilder, private router: Router) { // Inyecta el Router
     this.loginForm = this.formBuilder.group({
@@ -19,11 +20,11 @@ export class LoginComponent {
 
   onSubmit() {
     const { username, password } = this.loginForm.value;
-
+  
     if (username === 'yorlin' && password === 'yorlin') { // Valida las credenciales
       this.router.navigate(['/home']); // Redirige a la página '/home' en caso de éxito
     } else {
-      alert('Error de logueo: usuario o contraseña incorrectos'); // Muestra un mensaje de error
+      this.error = true; // Muestra el mensaje de error
     }
   }
 }
